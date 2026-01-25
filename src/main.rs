18d18eb6c607815d32b7a2ef56fe9643036b6db3
Jonas Elhs <jonas.elhs@outlook.com>
2026-01-25 15:22:39 +0100

    feat(main): add cli parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+#[derive(Subcommand)]
+enum Commands {
+    List {
+        /// Project to list the possible values for
+        project: Option<String>,
+    },
+    Generate {
+        /// Project to generate the templates from
+        project: String,
+        /// Values to supply to the templates
+        values: String,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::List { project } => {
+            println!("Listed '{}'", project.unwrap_or_else(|| "null".into()))
+        }
+        Commands::Generate { project, values } => {
+            println!("Generated project: '{}', values: '{}'", project, values)
+        }
+    }
 }
